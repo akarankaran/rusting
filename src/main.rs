c2b1e53c8142da591e123c90f28321aebbf743df
Karan <karan.panchal@lendingkart.com>
2025-01-08 16:50:03 +0530

    initial commit

--- src/main.rs
+++ src/main.rs
@@ -1,54 +1,17 @@
-fn main() {
-    welcome();
-}
+let x = 5;
+println!("{}", x);
 
-fn welcome() {
-    println!("Welcome to Rust!");
-}
+let name = "Alice";
+println!("{}", name);
 
-// Variation 1
-fn main() {
-    greet();
-}
+let pi = 3.14;
+println!("{}", pi);
 
-fn greet() {
-    println!("Welcome to Rust!");
-}
+let is_raining = false;
+println!("{}", is_raining);
 
-// Variation 2
-fn main() {
-    let message = create_welcome_message();
-    println!("{}", message);
-}
+let numbers = [1, 2, 3, 4, 5];
+println!("{:?}", numbers);
 
-fn create_welcome_message() -> &'static str {
-    "Welcome to Rust!"
-}
-
-// Variation 3
-fn main() {
-    print_welcome();
-}
-
-fn print_welcome() {
-    println!("Welcome to Rust!");
-}
-
-// Variation 4
-fn main() {
-    show_message("Welcome to Rust!");
-}
-
-fn show_message(msg: &str) {
-    println!("{}", msg);
-}
-
-// Variation 5
-fn main() {
-    display_welcome();
-}
-
-fn display_welcome() {
-    let welcome_text = "Welcome to Rust!";
-    println!("{}", welcome_text);
-}
+let greeting = String::from("Hello, World!");
+println!("{}", greeting);
