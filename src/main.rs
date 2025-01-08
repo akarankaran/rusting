7cbe9b1bcb0b43d8359c90f2f2c395428d67eca1
Karan <karan.panchal@lendingkart.com>
2025-01-08 16:54:32 +0530

    initial commit

--- src/main.rs
+++ src/main.rs
@@ -1,32 +1,23 @@
-let x = 5;
-println!("{}", x);
+let immutable_value = 42;
+println!("{}", immutable_value);
 
-let pi = 3.14;
-println!("{}", pi);
-
-let greeting = "Hello, World!";
+let greeting = "Hello, world!";
 println!("{}", greeting);
+
+let pi = 3.14159;
+println!("{}", pi);
 
 let is_rust_fun = true;
 println!("{}", is_rust_fun);
 
-let character = 'R';
-println!("{}", character);
+let hex_color = (255, 0, 0);
+println!("{:?}", hex_color);
 
-let array = [1, 2, 3, 4, 5];
-println!("{:?}", array);
+let name = String::from("Rustacean");
+println!("{}", name);  
 
-let tuple = (42, "Answer");
-println!("{:?}", tuple);
+let numbers = [1, 2, 3, 4, 5];
+println!("{:?}", numbers);
 
-let hex_value = 0x2A;
-println!("{}", hex_value);
-
-let big_number: u64 = 1_000_000_000;
-println!("{}", big_number);
-
-let decimal_value: f32 = 10.99;
-println!("{}", decimal_value);
-
-let unit = ();
-println!("{:?}", unit);
+let user = ("Alice", 30);
+println!("{:?}", user);
