1be8fc9ca59c05045bf71552f9efee4f228115ac
Karan <karan146panchal@gmail.com>
2025-01-08 16:59:06 +0530

    initial commit

--- src/main.rs
+++ src/main.rs
@@ -1,23 +1,17 @@
-let immutable_value = 42;
-println!("{}", immutable_value);
+let x = 42;
+println!("{}", x);
+
+let pi = 3.14;
+println!("{}", pi);
 
 let greeting = "Hello, world!";
 println!("{}", greeting);
 
-let pi = 3.14159;
-println!("{}", pi);
-
-let is_rust_fun = true;
-println!("{}", is_rust_fun);
-
-let hex_color = (255, 0, 0);
-println!("{:?}", hex_color);
-
-let name = String::from("Rustacean");
-println!("{}", name);  
+let is_active = true;
+println!("{}", is_active);
 
 let numbers = [1, 2, 3, 4, 5];
 println!("{:?}", numbers);
 
-let user = ("Alice", 30);
-println!("{:?}", user);
+let a: (i32, f64) = (5, 2.5);
+println!("{:?}", a);
