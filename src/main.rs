a57833054d0bd04d4a9e58db73188d9fb1904dd3
Karan <karan146panchal@gmail.com>
2025-01-09 15:18:31 +0530

    rust basics

--- src/main.rs
+++ src/main.rs
@@ -1,35 +1,10 @@
-use std::io;
-use rand::Rng;
-
 fn main() {
-    let secret_number = rand::thread_rng().gen_range(1..101);
-    let mut guess = String::new();
-    
-    loop {
-        println!("Please input your guess.");
-        
-        io::stdin()
-            .read_line(&mut guess)
-            .expect("Failed to read line");
-        
-        let guess: u32 = match guess.trim().parse() {
-            Ok(num) => num,
-            Err(_) => {
-                println!("Please enter a valid number.");
-                guess.clear();
-                continue;
-            },
-        };
-        
-        if guess < secret_number {
-            println!("Too small!");
-        } else if guess > secret_number {
-            println!("Too big!");
-        } else {
-            println!("You guessed it! The number was {}.", secret_number);
-            break;
-        }
-        
-        guess.clear();
+    let numbers = [1, 2, 3, 4, 5];
+    let mut product = 1;
+
+    for &num in &numbers {
+        product *= num;
     }
+
+    println!("The product of all numbers in the array is: {}", product);
 }
