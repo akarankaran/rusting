9aa23ab58754ddb2b32b559cead98b42cab44fcf
Karan <karan146panchal@gmail.com>
2025-01-09 11:48:28 +0530

    rust basics

--- src/main.rs
+++ src/main.rs
@@ -1,14 +1,6 @@
-use std::io;
-
 fn main() {
-    let mut input = String::new();
-    println!("Enter a year:");
-    io::stdin().read_line(&mut input).expect("Failed to read line");
-    let year: i32 = input.trim().parse().expect("Please enter a valid number");
-
-    if (year % 4 == 0 && year % 100 != 0) || (year % 400 == 0) {
-        println!("{} is a leap year.", year);
-    } else {
-        println!("{} is not a leap year.", year);
-    }
+    let string1 = String::from("Hello, ");
+    let string2 = String::from("world!");
+    let concatenated = string1 + &string2;
+    println!("{}", concatenated);
 }
