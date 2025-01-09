d1aa4a404e98853c496e42d7ec133564db306ef0
Karan <karan146panchal@gmail.com>
2025-01-09 16:56:15 +0530

    rust basics

--- src/main.rs
+++ src/main.rs
@@ -1,9 +1,15 @@
 fn main() {
-    let height = 5;
-    for i in 1..=height {
-        for j in 1..=i {
-            print!("* ");
-        }
-        println!();
+    let a = 56;
+    let b = 98;
+    let gcd = gcd(a, b);
+    println!("The GCD of {} and {} is {}", a, b, gcd);
+}
+
+fn gcd(mut a: i32, mut b: i32) -> i32 {
+    while b != 0 {
+        let temp = b;
+        b = a % b;
+        a = temp;
     }
+    a
 }
