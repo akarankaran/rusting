24a8cb7377ca7a7f79dcec27f492a395951fa234
Karan <karan146panchal@gmail.com>
2025-01-09 13:34:31 +0530

    rust basics

--- src/main.rs
+++ src/main.rs
@@ -1,23 +1,5 @@
 fn main() {
-    let a = 10;
-    let b = 20;
-    let c = 15;
-
-    let largest;
-
-    if a > b {
-        if a > c {
-            largest = a;
-        } else {
-            largest = c;
-        }
-    } else {
-        if b > c {
-            largest = b;
-        } else {
-            largest = c;
-        }
+    for i in 1..=10 {
+        println!("{}", i);
     }
-
-    println!("The largest number is: {}", largest);
 }
