0f254e5265f1441510690ae85ce2e946aa60edc0
Karan <karan146panchal@gmail.com>
2025-01-09 13:49:38 +0530

    rust basics

--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,13 @@
 fn main() {
-    for i in 1..=10 {
-        println!("{}", i);
+    let mut sum = 0;
+    let mut count = 0;
+    let mut number = 2;
+
+    while count < 10 {
+        sum += number;
+        number += 2;
+        count += 1;
     }
+
+    println!("The sum of the first 10 even numbers is: {}", sum);
 }
