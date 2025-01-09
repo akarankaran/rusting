b139db7d176a162b0368fc3606573b0d996703c0
Karan <karan146panchal@gmail.com>
2025-01-09 15:54:51 +0530

    rust basics

--- src/main.rs
+++ src/main.rs
@@ -1,21 +1,10 @@
 fn main() {
-    let number = 29;
-    let mut is_prime = true;
-
-    if number < 2 {
-        is_prime = false;
-    } else {
-        for i in 2..=(number as f64).sqrt() as usize {
-            if number % i == 0 {
-                is_prime = false;
-                break;
-            }
+    let mut count = 10;
+    loop {
+        if count == 0 {
+            break;
         }
-    }
-
-    if is_prime {
-        println!("{} is a prime number.", number);
-    } else {
-        println!("{} is not a prime number.", number);
+        println!("{}", count);
+        count -= 1;
     }
 }
