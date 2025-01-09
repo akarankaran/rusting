30b10844c49aed098e328516a998b24be3d520a8
Karan <karan146panchal@gmail.com>
2025-01-09 15:04:24 +0530

    rust basics

--- src/main.rs
+++ src/main.rs
@@ -1,12 +1,35 @@
+use std::io;
+use rand::Rng;
+
 fn main() {
-    let mut number = 12345;
-    let mut reversed = 0;
-
-    while number != 0 {
-        let digit = number % 10;
-        reversed = reversed * 10 + digit;
-        number /= 10;
+    let secret_number = rand::thread_rng().gen_range(1..101);
+    let mut guess = String::new();
+    
+    loop {
+        println!("Please input your guess.");
+        
+        io::stdin()
+            .read_line(&mut guess)
+            .expect("Failed to read line");
+        
+        let guess: u32 = match guess.trim().parse() {
+            Ok(num) => num,
+            Err(_) => {
+                println!("Please enter a valid number.");
+                guess.clear();
+                continue;
+            },
+        };
+        
+        if guess < secret_number {
+            println!("Too small!");
+        } else if guess > secret_number {
+            println!("Too big!");
+        } else {
+            println!("You guessed it! The number was {}.", secret_number);
+            break;
+        }
+        
+        guess.clear();
     }
-
-    println!("{}", reversed);
 }
