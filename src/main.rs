feb0ab2667348788a51f3398f6b63326e7cf51f3
Karan <karan146panchal@gmail.com>
2025-01-09 14:37:09 +0530

    rust basics

--- src/main.rs
+++ src/main.rs
@@ -1,10 +1,9 @@
 fn main() {
-    let grade = 85;
-
-    match grade {
-        90..=100 => println!("Excellent"),
-        75..=89 => println!("Good"),
-        0..=74 => println!("Poor"),
-        _ => println!("Invalid grade"),
+    let mut sum = 0;
+    for i in 1..=50 {
+        if i % 2 != 0 {
+            sum += i;
+        }
     }
+    println!("{}", sum);
 }
