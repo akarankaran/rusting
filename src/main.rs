b9aea4ff8fd3314365f2d0ef0b4fa9ffb856b779
Karan <karan146panchal@gmail.com>
2025-01-10 12:47:58 +0530

    rust basics

--- src/main.rs
+++ src/main.rs
@@ -1,11 +1,6 @@
 fn main() {
-    let mut vec = vec![1, 2, 3];
-    add_elements(&mut vec);
-    println!("{:?}", vec);
-}
-
-fn add_elements(vec: &mut Vec<i32>) {
-    vec.push(4);
-    vec.push(5);
-    vec.push(6);
+    let original = vec![1, 2, 3, 4, 5];
+    let cloned = original.clone();
+    println!("Original: {:?}", original);
+    println!("Cloned: {:?}", cloned);
 }
