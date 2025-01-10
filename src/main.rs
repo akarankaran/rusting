df9c09a5f1e77afd11109da6c5b649774c392362
Karan <karan146panchal@gmail.com>
2025-01-10 13:23:17 +0530

    rust basics

--- src/main.rs
+++ src/main.rs
@@ -1,11 +1,9 @@
 fn main() {
-    let mut vec = vec![1, 2, 3, 4, 5];
-    swap(&mut vec, 1, 3);
-    println!("{:?}", vec);
+    let s = String::from("Hello, world!");
+    let length = calculate_length(&s);
+    println!("The length of '{}' is {}.", s, length);
 }
 
-fn swap<T>(vec: &mut Vec<T>, a: usize, b: usize) {
-    let temp = std::mem::take(&mut vec[a]);
-    vec[a] = std::mem::take(&mut vec[b]);
-    vec[b] = temp;
+fn calculate_length(s: &String) -> usize {
+    s.len()
 }
