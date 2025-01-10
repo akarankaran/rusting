9a52639d013c2861e29d246197a8ff905eb4cdd1
Karan <karan146panchal@gmail.com>
2025-01-10 13:37:23 +0530

    rust basics

--- src/main.rs
+++ src/main.rs
@@ -1,9 +1,23 @@
 fn main() {
-    let s = String::from("Hello, world!");
-    let length = calculate_length(&s);
-    println!("The length of '{}' is {}.", s, length);
-}
+    let a = 10;
+    let b = 20;
+    let c = 30;
 
-fn calculate_length(s: &String) -> usize {
-    s.len()
+    let (ref x, ref y, ref z) = (&a, &b, &c);
+
+    println!("x: {}, y: {}, z: {}", x, y, z);
+
+    let d = 40;
+    let e = 50;
+
+    let (ref p1, ref p2) = (&d, &e);
+    
+    println!("p1: {}, p2: {}", p1, p2);
+    
+    let x2 = 5;
+    let y2 = 15;
+
+    let (ref a2, ref b2) = if true { (&x2, &y2) } else { (&x2, &y2) };
+    
+    println!("a2: {}, b2: {}", a2, b2);
 }
