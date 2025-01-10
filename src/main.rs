381b5b91ab9cf0f8c31d830882198f8f4bc61ff4
Karan <karan146panchal@gmail.com>
2025-01-10 12:05:34 +0530

    rust basics

--- src/main.rs
+++ src/main.rs
@@ -1,40 +1,9 @@
 fn main() {
-    let s1 = String::from("Hello");
-    let s2 = s1;
-    
-    println!("{}", s2);
-    
-    let a = Box::new(5);
-    let b = a;
-    
-    println!("{}", b);
-    
-    let x = String::from("Ownership");
-    take_ownership(x);
-    
-    // Uncommenting the next line will cause an error because x has been moved
-    // println!("{}", x);
-    
-    let y = String::from("Borrowed");
-    let z = borrow_string(&y);
-    
-    println!("{}", y);
-    println!("{}", z);
-    
-    let v1 = vec![1, 2, 3];
-    let v2 = move_vec(v1);
-    
-    println!("{:?}", v2);
+    let s = String::from("Hello, world!");
+    let length = print_length(&s);
+    println!("The length of the string is: {}", length);
 }
 
-fn take_ownership(s: String) {
-    println!("{}", s);
-}
-
-fn borrow_string(s: &String) -> String {
-    s.clone()
-}
-
-fn move_vec(v: Vec<i32>) -> Vec<i32> {
-    v
+fn print_length(s: &str) -> usize {
+    s.len()
 }
