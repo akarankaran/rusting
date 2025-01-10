7f4fa51297c64f79bca903fdfd3505d39bee7af8
Karan <karan146panchal@gmail.com>
2025-01-10 12:16:38 +0530

    rust basics

--- src/main.rs
+++ src/main.rs
@@ -1,9 +1,11 @@
-fn main() {
-    let s = String::from("Hello, world!");
-    let length = print_length(&s);
-    println!("The length of the string is: {}", length);
+fn first_character(s: &str) -> Option<char> {
+    s.chars().next()
 }
 
-fn print_length(s: &str) -> usize {
-    s.len()
+fn main() {
+    let example_string = "Hello, world!";
+    match first_character(example_string) {
+        Some(c) => println!("The first character is: {}", c),
+        None => println!("The string is empty."),
+    }
 }
