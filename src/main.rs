a1f54dde1f218712d4be4f0db0197a2d4086db1f
Karan <karan146panchal@gmail.com>
2025-01-10 11:41:23 +0530

    rust basics

--- src/main.rs
+++ src/main.rs
@@ -1,15 +1,23 @@
-fn main() {
-    let a = 56;
-    let b = 98;
-    let gcd = gcd(a, b);
-    println!("The GCD of {} and {} is {}", a, b, gcd);
-}
+use std::io;
 
-fn gcd(mut a: i32, mut b: i32) -> i32 {
-    while b != 0 {
-        let temp = b;
-        b = a % b;
-        a = temp;
+fn main() {
+    let mut input = String::new();
+    println!("Enter a number to convert to binary:");
+    io::stdin().read_line(&mut input).expect("Failed to read line");
+    let number: u32 = input.trim().parse().expect("Please enter a valid number");
+    let mut binary = Vec::new();
+    
+    let mut num = number;
+    if num == 0 {
+        binary.push('0');
+    } else {
+        while num > 0 {
+            binary.push(if num % 2 == 0 { '0' } else { '1' });
+            num /= 2;
+        }
     }
-    a
+    
+    binary.reverse();
+    let binary_string: String = binary.iter().collect();
+    println!("Binary representation of {} is: {}", number, binary_string);
 }
