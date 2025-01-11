fee5c05c89aab048615c773af194184c372d1307
Karan <karan146panchal@gmail.com>
2025-01-11 18:01:09 +0530

    rust basics

--- src/main.rs
+++ src/main.rs
@@ -1,12 +1,15 @@
-fn main() {
-    let s1 = String::from("Hello, ");
-    let s2 = String::from("world!");
-    let s3 = String::from(" How are you?");
-    
-    let combined = combine_strings(&s1, &s2, &s3);
-    println!("{}", combined);
+fn largest<T: PartialOrd>(vec: &[T]) -> &T {
+    let mut largest = &vec[0];
+    for item in vec {
+        if item > largest {
+            largest = item;
+        }
+    }
+    largest
 }
 
-fn combine_strings<'a>(s1: &'a str, s2: &'a str, s3: &'a str) -> String {
-    format!("{}{}{}", s1, s2, s3)
+fn main() {
+    let numbers = vec![34, 50, 25, 100, 65];
+    let result = largest(&numbers);
+    println!("The largest number is {}", result);
 }
