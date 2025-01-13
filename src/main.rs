a2babe5232ab7855c1e10435b4215f4d1fa4f918
Karan <karan146panchal@gmail.com>
2025-01-13 11:12:19 +0530

    rust basics

--- src/main.rs
+++ src/main.rs
@@ -1,20 +1,17 @@
-fn is_sorted(arr: &[i32]) -> bool {
-    for i in 1..arr.len() {
-        if arr[i] < arr[i - 1] {
-            return false;
-        }
-    }
-    true
-}
-
 fn main() {
-    let array1 = [1, 2, 3, 4, 5];
-    let array2 = [5, 4, 3, 2, 1];
-    let array3 = [1, 2, 2, 3, 4];
-    let array4: [i32; 0] = [];
+    struct Person {
+        name: String,
+        age: u32,
+        city: String,
+    }
 
-    println!("{}", is_sorted(&array1));
-    println!("{}", is_sorted(&array2));
-    println!("{}", is_sorted(&array3));
-    println!("{}", is_sorted(&array4));
+    let person = Person {
+        name: String::from("Alice"),
+        age: 30,
+        city: String::from("New York"),
+    };
+
+    println!("Name: {}", person.name);
+    println!("Age: {}", person.age);
+    println!("City: {}", person.city);
 }
