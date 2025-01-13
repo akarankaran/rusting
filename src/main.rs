43a17810d00bceba9e229ff508870aba936ffb83
Karan <karan146panchal@gmail.com>
2025-01-13 11:51:48 +0530

    rust basics

--- src/main.rs
+++ src/main.rs
@@ -1,35 +1,16 @@
-use std::fmt;
+use std::f64::consts::PI;
 
-struct Rectangle {
-    width: u32,
-    height: u32,
+struct Circle {
+    radius: f64,
 }
 
-impl Rectangle {
-    fn area(&self) -> u32 {
-        self.width * self.height
-    }
-}
-
-impl fmt::Display for Rectangle {
-    fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result {
-        write!(f, "Rectangle: {} x {}", self.width, self.height)
+impl Circle {
+    fn circumference(&self) -> f64 {
+        2.0 * PI * self.radius
     }
 }
 
 fn main() {
-    let rect1 = Rectangle { width: 30, height: 50 };
-    let rect2 = Rectangle { width: 10, height: 15 };
-
-    println!("{}", rect1);
-    println!("Area of rect1: {}", rect1.area());
-    
-    println!("{}", rect2);
-    println!("Area of rect2: {}", rect2.area());
-    
-    let rect3 = Rectangle { width: 12, height: 40 };
-    println!("Area of rect3: {}", rect3.area());
-
-    let rect4 = Rectangle { width: 20, height: 10 };
-    println!("Area of rect4: {}", rect4.area());
+    let circle = Circle { radius: 5.0 };
+    println!("Circumference of the circle: {}", circle.circumference());
 }
