3c14e0c1a521e9bc0c3f84c30ebde8a4adce2a6e
Karan <karan146panchal@gmail.com>
2025-01-13 15:37:51 +0530

    rust basics

--- src/main.rs
+++ src/main.rs
@@ -1,64 +1,17 @@
-use std::fmt;
+use std::vec::Vec;
 
-struct Point {
-    x: f64,
-    y: f64,
-    z: f64,
+struct IntegerVector {
+    numbers: Vec<i32>,
 }
 
-struct Color {
-    red: u8,
-    green: u8,
-    blue: u8,
-}
-
-struct Shape {
-    name: String,
-    color: Color,
-    position: Point,
-    volume: f64,
-}
-
-impl fmt::Display for Shape {
-    fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result {
-        write!(
-            f,
-            "Shape: {}\nColor: ({}, {}, {})\nPosition: ({}, {}, {})\nVolume: {}",
-            self.name,
-            self.color.red,
-            self.color.green,
-            self.color.blue,
-            self.position.x,
-            self.position.y,
-            self.position.z,
-            self.volume
-        )
+impl IntegerVector {
+    fn sum(&self) -> i32 {
+        self.numbers.iter().sum()
     }
 }
 
 fn main() {
-    let cube = Shape {
-        name: String::from("Cube"),
-        color: Color {
-            red: 0,
-            green: 255,
-            blue: 0,
-        },
-        position: Point { x: 0.0, y: 0.0, z: 0.0 },
-        volume: 27.0,
-    };
-
-    let sphere = Shape {
-        name: String::from("Sphere"),
-        color: Color {
-            red: 255,
-            green: 0,
-            blue: 0,
-        },
-        position: Point { x: 1.0, y: 1.0, z: 1.0 },
-        volume: 33.51,
-    };
-
-    println!("{}", cube);
-    println!("{}", sphere);
+    let vec = IntegerVector { numbers: vec![1, 2, 3, 4, 5] };
+    let total = vec.sum();
+    println!("The sum of the vector is: {}", total);
 }
