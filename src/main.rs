3f401b1ff6d72a375bfa261ebe27701426458b24
Karan <karan146panchal@gmail.com>
2025-01-13 12:27:17 +0530

    rust basics

--- src/main.rs
+++ src/main.rs
@@ -1,26 +1,21 @@
-use std::cmp;
+use std::f64;
 
-struct Rectangle {
-    width: u32,
-    height: u32,
+struct Point {
+    x: f64,
+    y: f64,
 }
 
-impl Rectangle {
-    fn can_fit_inside(&self, other: &Rectangle) -> bool {
-        (self.width <= other.width && self.height <= other.height) ||
-        (self.height <= other.width && self.width <= other.height)
+impl Point {
+    fn distance(&self, other: &Point) -> f64 {
+        let dx = self.x - other.x;
+        let dy = self.y - other.y;
+        (dx * dx + dy * dy).sqrt()
     }
 }
 
 fn main() {
-    let rect1 = Rectangle { width: 3, height: 4 };
-    let rect2 = Rectangle { width: 5, height: 6 };
-    let rect3 = Rectangle { width: 6, height: 5 };
-    
-    assert_eq!(rect1.can_fit_inside(&rect2), true);
-    assert_eq!(rect1.can_fit_inside(&rect3), true);
-    assert_eq!(rect2.can_fit_inside(&rect1), false);
-    assert_eq!(rect2.can_fit_inside(&rect3), true);
-    assert_eq!(rect3.can_fit_inside(&rect2), false);
-    assert_eq!(rect3.can_fit_inside(&rect1), false);
+    let point1 = Point { x: 1.0, y: 2.0 };
+    let point2 = Point { x: 4.0, y: 6.0 };
+    let dist = point1.distance(&point2);
+    println!("The distance between the points is: {}", dist);
 }
