1344a586537021474ee2307a7fe42671a142adbe
Karan <karan146panchal@gmail.com>
2025-01-13 10:59:09 +0530

    rust basics

--- src/main.rs
+++ src/main.rs
@@ -1,9 +1,20 @@
-fn main() {
-    let num = 5;
-    let square = calculate_square(num);
-    println!("The square of {} is {}", num, square);
+fn is_sorted(arr: &[i32]) -> bool {
+    for i in 1..arr.len() {
+        if arr[i] < arr[i - 1] {
+            return false;
+        }
+    }
+    true
 }
 
-fn calculate_square(n: i32) -> i32 {
-    n * n
+fn main() {
+    let array1 = [1, 2, 3, 4, 5];
+    let array2 = [5, 4, 3, 2, 1];
+    let array3 = [1, 2, 2, 3, 4];
+    let array4: [i32; 0] = [];
+
+    println!("{}", is_sorted(&array1));
+    println!("{}", is_sorted(&array2));
+    println!("{}", is_sorted(&array3));
+    println!("{}", is_sorted(&array4));
 }
