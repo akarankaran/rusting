6682ac00bf6d89b0a94ef2f11d77f86cbec9153c
Karan <karan146panchal@gmail.com>
2025-01-13 11:37:40 +0530

    rust basics

--- src/main.rs
+++ src/main.rs
@@ -1,50 +1,35 @@
 use std::fmt;
 
-struct Color(u8, u8, u8);
+struct Rectangle {
+    width: u32,
+    height: u32,
+}
 
-impl fmt::Display for Color {
-    fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result {
-        write!(f, "RGB({}, {}, {})", self.0, self.1, self.2)
+impl Rectangle {
+    fn area(&self) -> u32 {
+        self.width * self.height
     }
 }
 
-impl Color {
-    fn from_hex(hex: &str) -> Option<Color> {
-        let hex = hex.trim_start_matches('#');
-        if hex.len() != 6 {
-            return None;
-        }
-        let r = u8::from_str_radix(&hex[0..2], 16).ok()?;
-        let g = u8::from_str_radix(&hex[2..4], 16).ok()?;
-        let b = u8::from_str_radix(&hex[4..6], 16).ok()?;
-        Some(Color(r, g, b))
-    }
-
-    fn to_hex(&self) -> String {
-        format!("#{:02X}{:02X}{:02X}", self.0, self.1, self.2)
-    }
-
-    fn blend(&self, other: &Color, ratio: f32) -> Color {
-        let r = (self.0 as f32 * (1.0 - ratio) + other.0 as f32 * ratio).round() as u8;
-        let g = (self.1 as f32 * (1.0 - ratio) + other.1 as f32 * ratio).round() as u8;
-        let b = (self.2 as f32 * (1.0 - ratio) + other.2 as f32 * ratio).round() as u8;
-        Color(r, g, b)
+impl fmt::Display for Rectangle {
+    fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result {
+        write!(f, "Rectangle: {} x {}", self.width, self.height)
     }
 }
 
 fn main() {
-    let color1 = Color(255, 0, 0);
-    let color2 = Color(0, 0, 255);
-    println!("Color 1: {}", color1);
-    println!("Color 2: {}", color2);
+    let rect1 = Rectangle { width: 30, height: 50 };
+    let rect2 = Rectangle { width: 10, height: 15 };
 
-    let blended_color = color1.blend(&color2, 0.5);
-    println!("Blended Color: {}", blended_color);
+    println!("{}", rect1);
+    println!("Area of rect1: {}", rect1.area());
+    
+    println!("{}", rect2);
+    println!("Area of rect2: {}", rect2.area());
+    
+    let rect3 = Rectangle { width: 12, height: 40 };
+    println!("Area of rect3: {}", rect3.area());
 
-    if let Some(hex_color) = Color::from_hex("#FF5733") {
-        println!("Hex to Color: {}", hex_color);
-        println!("Color to Hex: {}", hex_color.to_hex());
-    } else {
-        println!("Invalid hex color.");
-    }
+    let rect4 = Rectangle { width: 20, height: 10 };
+    println!("Area of rect4: {}", rect4.area());
 }
