f2e56f62d3931aa206ffc050dbbdcf0eeac40832
Karan <karan146panchal@gmail.com>
2025-01-15 14:59:01 +0530

    rust basics

--- src/main.rs
+++ src/main.rs
@@ -1,58 +1,33 @@
 fn main() {
-    let mut vec = Vec::new();
+    let vector = vec![1, 3, 5, 2, 4];
+    let max_value = vector.iter().cloned().max().unwrap();
+    println!("The maximum value is: {}", max_value);
     
-    vec.push(10);
-    vec.push(20);
-    vec.push(30);
+    let empty_vector: Vec<i32> = Vec::new();
+    let max_empty = empty_vector.iter().cloned().max().unwrap_or(0);
+    println!("The maximum value in empty vector is: {}", max_empty);
     
-    println!("{:?}", vec);
+    let negative_vector = vec![-1, -2, -3, -4, -5];
+    let max_negative = negative_vector.iter().cloned().max().unwrap();
+    println!("The maximum value in negative vector is: {}", max_negative);
+
+    let mix_vector = vec![1, 2, -3, 4, -5];
+    let max_mixed = mix_vector.iter().cloned().max().unwrap();
+    println!("The maximum value in mixed vector is: {}", max_mixed);
     
-    vec.pop();
+    let large_vector: Vec<i32> = (0..1000).collect();
+    let max_large = large_vector.iter().cloned().max().unwrap();
+    println!("The maximum value in large vector is: {}", max_large);
     
-    println!("{:?}", vec);
+    let duplicate_vector = vec![1, 2, 3, 2, 1];
+    let max_duplicates = duplicate_vector.iter().cloned().max().unwrap();
+    println!("The maximum value in duplicate vector is: {}", max_duplicates);
     
-    vec.insert(1, 15);
+    let float_vector = vec![1.1, 2.2, 3.3, 2.2, 1.1];
+    let max_float = float_vector.iter().cloned().fold(f32::MIN, f32::max);
+    println!("The maximum value in float vector is: {}", max_float);
     
-    println!("{:?}", vec);
-    
-    if let Some(pos) = vec.iter().position(|&x| x == 20) {
-        vec.remove(pos);
-    }
-    
-    println!("{:?}", vec);
-    
-    vec.extend(vec![40, 50, 60]);
-    
-    println!("{:?}", vec);
-    
-    vec.clear();
-    
-    println!("{:?}", vec);
-    
-    vec.push(100);
-    vec.push(200);
-    
-    println!("{:?}", vec);
-    
-    let mut vec2 = vec.clone();
-    
-    vec2.sort();
-    
-    println!("{:?}", vec2);
-    
-    vec.reverse();
-    
-    println!("{:?}", vec);
-    
-    let first = vec.get(0);
-    
-    println!("{:?}", first);
-    
-    let last = vec.pop();
-    
-    println!("{:?}", last);
-    
-    vec.push(300);
-    
-    println!("{:?}", vec);
+    let large_float_vector: Vec<f64> = (0..1000).map(|x| x as f64 * 1.1).collect();
+    let max_large_float = large_float_vector.iter().cloned().max().unwrap();
+    println!("The maximum value in large float vector is: {}", max_large_float);
 }
