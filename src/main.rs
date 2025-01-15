2cd856ef24fb974d9d9b743e2c49cce7e77cdb8c
Karan <karan146panchal@gmail.com>
2025-01-15 14:43:52 +0530

    rust basics

--- src/main.rs
+++ src/main.rs
@@ -1,12 +1,58 @@
-use std::vec::Vec;
-
 fn main() {
-    let mut numbers: Vec<i32> = Vec::new();
-    for i in 1..=10 {
-        numbers.push(i);
+    let mut vec = Vec::new();
+    
+    vec.push(10);
+    vec.push(20);
+    vec.push(30);
+    
+    println!("{:?}", vec);
+    
+    vec.pop();
+    
+    println!("{:?}", vec);
+    
+    vec.insert(1, 15);
+    
+    println!("{:?}", vec);
+    
+    if let Some(pos) = vec.iter().position(|&x| x == 20) {
+        vec.remove(pos);
     }
-
-    for number in &numbers {
-        println!("{}", number);
-    }
+    
+    println!("{:?}", vec);
+    
+    vec.extend(vec![40, 50, 60]);
+    
+    println!("{:?}", vec);
+    
+    vec.clear();
+    
+    println!("{:?}", vec);
+    
+    vec.push(100);
+    vec.push(200);
+    
+    println!("{:?}", vec);
+    
+    let mut vec2 = vec.clone();
+    
+    vec2.sort();
+    
+    println!("{:?}", vec2);
+    
+    vec.reverse();
+    
+    println!("{:?}", vec);
+    
+    let first = vec.get(0);
+    
+    println!("{:?}", first);
+    
+    let last = vec.pop();
+    
+    println!("{:?}", last);
+    
+    vec.push(300);
+    
+    println!("{:?}", vec);
 }
