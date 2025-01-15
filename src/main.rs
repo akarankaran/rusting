697dd978c9a6f6c5dbd0b7f72542ecda3bb30025
Karan <karan146panchal@gmail.com>
2025-01-15 13:11:53 +0530

    rust basics

--- src/main.rs
+++ src/main.rs
@@ -1,41 +1,23 @@
+fn split_array<T>(array: &[T]) -> (Vec<T>, Vec<T>) 
+where 
+    T: Clone,
+{
+    let mid = array.len() / 2;
+    let first_half = array[..mid].to_vec();
+    let second_half = array[mid..].to_vec();
+    (first_half, second_half)
+}
+
 fn main() {
-    let array1 = [1, 2, 3];
-    let array2 = [4, 5, 6];
-    let merged: Vec<i32> = array1.iter().cloned().chain(array2.iter().cloned()).collect();
-    println!("{:?}", merged);
-    
-    let array1 = ["apple", "banana"];
-    let array2 = ["orange", "pear"];
-    let merged_fruits: Vec<&str> = array1.iter().cloned().chain(array2.iter().cloned()).collect();
-    println!("{:?}", merged_fruits);
+    let array = vec![1, 2, 3, 4, 5, 6];
+    let (first_half, second_half) = split_array(&array);
+    println!("{:?}, {:?}", first_half, second_half);
 
-    let array1 = [true, false];
-    let array2 = [false, true];
-    let merged_bools: Vec<bool> = array1.iter().cloned().chain(array2.iter().cloned()).collect();
-    println!("{:?}", merged_bools);
+    let string_array = vec!["a", "b", "c", "d", "e"];
+    let (first_half_strings, second_half_strings) = split_array(&string_array);
+    println!("{:?}, {:?}", first_half_strings, second_half_strings);
 
-    let mut array1 = vec![1, 2, 3];
-    let array2 = vec![4, 5, 6];
-    array1.extend(array2);
-    println!("{:?}", array1);
-    
-    let array1 = [1.1, 2.2, 3.3];
-    let array2 = [4.4, 5.5];
-    let merged_floats: Vec<f64> = array1.iter().cloned().chain(array2.iter().cloned()).collect();
-    println!("{:?}", merged_floats);
-    
-    let array1 = ['a', 'b'];
-    let array2 = ['c', 'd', 'e'];
-    let merged_chars: Vec<char> = array1.iter().cloned().chain(array2.iter().cloned()).collect();
-    println!("{:?}", merged_chars);
-    
-    let array1: [i32; 0] = [];
-    let array2 = [7, 8, 9];
-    let merged_empty: Vec<i32> = array1.iter().cloned().chain(array2.iter().cloned()).collect();
-    println!("{:?}", merged_empty);
-    
-    let array1 = ["rust", "is"];
-    let array2 = ["awesome"];
-    let merged_strings: Vec<&str> = array1.iter().cloned().chain(array2.iter().cloned()).collect();
-    println!("{:?}", merged_strings);
+    let mixed_array: Vec<Box<dyn std::any::Any>> = vec![Box::new(1), Box::new("hello"), Box::new(3.14)];
+    let (first_half_mixed, second_half_mixed) = split_array(&mixed_array);
+    println!("{:?}, {:?}", first_half_mixed.len(), second_half_mixed.len());
 }
