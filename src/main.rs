0d695b0ceb75856c15d92d7daf6c959b7460db44
Karan <karan146panchal@gmail.com>
2025-01-16 13:17:18 +0530

    rust basics

--- src/main.rs
+++ src/main.rs
@@ -1,45 +1,20 @@
+use std::vec::Vec;
+
 fn main() {
-    let some_value = Some(10);
-    if let Some(val) = some_value {
-        println!("Value is: {}", val);
-    }
+    let vec1: Vec<i32> = vec![1, 2, 3, 4, 5];
+    let vec2: Vec<i32> = vec![];
+    let vec3: Vec<i32> = vec![10, 20, 30];
 
-    let none_value: Option<i32> = None;
-    if let Some(val) = none_value {
-        println!("This won't print: {}", val);
-    } else {
-        println!("No value found.");
-    }
+    check_vector_length(&vec1);
+    check_vector_length(&vec2);
+    check_vector_length(&vec3);
+}
 
-    let some_string = Some("Hello");
-    if let Some(text) = some_string {
-        println!("{}", text);
-    }
-
-    let none_string: Option<&str> = None;
-    if let Some(text) = none_string {
-        println!("{}", text);
-    } else {
-        println!("No text found.");
-    }
-
-    let some_float: Option<f64> = Some(3.14);
-    if let Some(num) = some_float {
-        println!("Float value: {}", num);
-    }
-
-    let arr: [Option<i32>; 5] = [Some(1), None, Some(2), None, Some(3)];
-    for item in arr.iter() {
-        if let Some(value) = item {
-            println!("Array value: {}", value);
-        }
-    }
-
-    let map = std::collections::HashMap::new();
-    let some_key = "key";
-    if let Some(value) = map.get(some_key) {
-        println!("Found in map: {}", value);
-    } else {
-        println!("Key not found in map.");
+fn check_vector_length(vec: &Vec<i32>) {
+    match vec.len() {
+        0 => println!("The vector is empty."),
+        1 => println!("The vector has one element."),
+        2..=10 => println!("The vector has between 2 and 10 elements."),
+        _ => println!("The vector has more than 10 elements."),
     }
 }
