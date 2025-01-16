49ba18f490b653063a64e0d9d950f84010c78150
Karan <karan146panchal@gmail.com>
2025-01-16 12:24:24 +0530

    rust basics

--- src/main.rs
+++ src/main.rs
@@ -1,39 +1,33 @@
+fn sum_nested_tuple(tuple: &dyn std::any::Any) -> i32 {
+    match tuple.downcast_ref::<(i32, i32)>() {
+        Some(&(a, b)) => a + b,
+        None => {
+            match tuple.downcast_ref::<((i32, i32), i32)>() {
+                Some(&((a, b), c)) => a + b + c,
+                None => {
+                    match tuple.downcast_ref::<(i32, (i32, i32))>() {
+                        Some(&(a, (b, c))) => a + b + c,
+                        None => {
+                            match tuple.downcast_ref::<((i32, (i32, i32)), i32)>() {
+                                Some(&((a, (b, c)), d)) => a + b + c + d,
+                                None => 0,
+                            }
+                        }
+                    }
+                }
+            }
+        }
+    }
+}
+
 fn main() {
-    let tuple1 = (1, "Hello", 3.14);
-    match tuple1 {
-        (x, y, z) => {
-            println!("Integer: {}, String: {}, Float: {}", x, y, z);
-        }
-    }
-
-    let tuple2 = (true, 'R', 42);
-    match tuple2 {
-        (a, b, c) => {
-            println!("Boolean: {}, Char: {}, Number: {}", a, b, c);
-        }
-    }
-
-    let tuple3 = (String::from("Rust"), 7);
-    match tuple3 {
-        (s, n) => {
-            println!("String: {}, Integer: {}", s, n);
-        }
-    }
-
-    let tuple4 = (0.5, "Match", true);
-    match tuple4 {
-        (f, str, b) => {
-            println!("Float: {}, String: {}, Boolean: {}", f, str, b);
-        }
-    }
-
-    let tuple5 = (10, 20, 30);
-    match tuple5 {
-        (a, b, c) if a + b + c > 50 => {
-            println!("Sum is greater than 50: {}", a + b + c);
-        }
-        (a, b, c) => {
-            println!("Sum is 50 or less: {}", a + b + c);
-        }
-    }
+    let nested_tuple_1: &(dyn std::any::Any) = &(1, 2);
+    let nested_tuple_2: &(dyn std::any::Any) = &((1, 2), 3);
+    let nested_tuple_3: &(dyn std::any::Any) = &(1, (2, 3));
+    let nested_tuple_4: &(dyn std::any::Any) = &((1, (2, 3)), 4);
+    
+    println!("{}", sum_nested_tuple(nested_tuple_1));
+    println!("{}", sum_nested_tuple(nested_tuple_2));
+    println!("{}", sum_nested_tuple(nested_tuple_3));
+    println!("{}", sum_nested_tuple(nested_tuple_4));
 }
