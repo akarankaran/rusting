2e4348c2a4976f22e13476f56bce461b46735c6f
Karan <karan146panchal@gmail.com>
2025-01-16 16:34:53 +0530

    rust basics

--- src/main.rs
+++ src/main.rs
@@ -1,58 +1,63 @@
+use std::vec::Vec;
+
 fn main() {
-    let value = 42;
+    let vec1 = vec![1, 2, 3];
+    let vec2 = vec![1, 2, 4];
 
-    match value {
-        1 => println!("One"),
-        2 => println!("Two"),
-        3 => println!("Three"),
-        _ => println!("Not one, two, or three"),
+    let result: Vec<String> = vec1.iter().zip(vec2.iter())
+        .map(|(a, b)| match a.cmp(b) {
+            std::cmp::Ordering::Less => format!("{} is less than {}", a, b),
+            std::cmp::Ordering::Greater => format!("{} is greater than {}", a, b),
+            std::cmp::Ordering::Equal => format!("{} is equal to {}", a, b),
+        })
+        .collect();
+
+    for res in result {
+        println!("{}", res);
     }
 
-    let option = Some(5);
+    let vec3 = vec![3, 3, 3];
+    let vec4 = vec![3, 3, 3];
 
-    match option {
-        Some(x) if x > 0 => println!("Positive {}", x),
-        Some(_) => println!("Non-positive"),
-        None => println!("No value"),
+    let result2: Vec<String> = vec3.iter().zip(vec4.iter())
+        .map(|(a, b)| match a.cmp(b) {
+            std::cmp::Ordering::Less => format!("{} is less than {}", a, b),
+            std::cmp::Ordering::Greater => format!("{} is greater than {}", a, b),
+            std::cmp::Ordering::Equal => format!("{} is equal to {}", a, b),
+        })
+        .collect();
+
+    for res in result2 {
+        println!("{}", res);
     }
 
-    let tuple = (1, 2);
+    let vec5 = vec!['a', 'b', 'c'];
+    let vec6 = vec!['a', 'b', 'd'];
 
-    match tuple {
-        (0, y) => println!("First is zero, second is {}", y),
-        (x, 0) => println!("First is {}, second is zero", x),
-        (x, y) => println!("First is {}, second is {}", x, y),
+    let result3: Vec<String> = vec5.iter().zip(vec6.iter())
+        .map(|(a, b)| match a.cmp(b) {
+            std::cmp::Ordering::Less => format!("{} is less than {}", a, b),
+            std::cmp::Ordering::Greater => format!("{} is greater than {}", a, b),
+            std::cmp::Ordering::Equal => format!("{} is equal to {}", a, b),
+        })
+        .collect();
+
+    for res in result3 {
+        println!("{}", res);
     }
 
-    let character = 'c';
+    let vec7 = vec![true, false, true];
+    let vec8 = vec![false, false, true];
 
-    match character {
-        'a' | 'b' | 'c' => println!("Found a, b, or c"),
-        _ => println!("Other character"),
-    }
+    let result4: Vec<String> = vec7.iter().zip(vec8.iter())
+        .map(|(a, b)| match a.cmp(b) {
+            std::cmp::Ordering::Less => format!("{:?} is less than {:?}", a, b),
+            std::cmp::Ordering::Greater => format!("{:?} is greater than {:?}", a, b),
+            std::cmp::Ordering::Equal => format!("{:?} is equal to {:?}", a, b),
+        })
+        .collect();
 
-    let number = -1;
-
-    match number {
-        n if n < 0 => println!("Negative number"),
-        n if n == 0 => println!("Zero"),
-        n => println!("Positive number: {}", n),
-    }
-
-    let random_value = "hello";
-
-    match random_value {
-        "hello" => println!("Hello there!"),
-        "world" => println!("World!"),
-        _ => println!("Unknown greeting"),
-    }
-
-    let list = vec![1, 2, 3];
-
-    match list.as_slice() {
-        [1, 2, ..] => println!("Starts with 1, 2"),
-        [1, ..] => println!("Starts with 1"),
-        [] => println!("Empty list"),
-        _ => println!("Some other list"),
+    for res in result4 {
+        println!("{}", res);
     }
 }
