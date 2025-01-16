4c40337cfa5b707ceda5e934b9a479e5924b59cc
Karan <karan146panchal@gmail.com>
2025-01-16 16:19:25 +0530

    rust basics

--- src/main.rs
+++ src/main.rs
@@ -1,25 +1,58 @@
 fn main() {
-    let array = [1, 2, 3, 4, 5, 6];
-    let middle = get_middle(&array);
-    println!("{:?}", middle);
+    let value = 42;
 
-    let array_odd = [10, 20, 30, 40, 50];
-    let middle_odd = get_middle(&array_odd);
-    println!("{:?}", middle_odd);
-
-    let empty_array: [i32; 0] = [];
-    let middle_empty = get_middle(&empty_array);
-    println!("{:?}", middle_empty);
-}
-
-fn get_middle(arr: &[i32]) -> Vec<i32> {
-    let len = arr.len();
-    if len == 0 {
-        return Vec::new();
+    match value {
+        1 => println!("One"),
+        2 => println!("Two"),
+        3 => println!("Three"),
+        _ => println!("Not one, two, or three"),
     }
-    if len % 2 == 0 {
-        return vec![arr[len / 2 - 1], arr[len / 2]];
-    } else {
-        return vec![arr[len / 2]];
+
+    let option = Some(5);
+
+    match option {
+        Some(x) if x > 0 => println!("Positive {}", x),
+        Some(_) => println!("Non-positive"),
+        None => println!("No value"),
+    }
+
+    let tuple = (1, 2);
+
+    match tuple {
+        (0, y) => println!("First is zero, second is {}", y),
+        (x, 0) => println!("First is {}, second is zero", x),
+        (x, y) => println!("First is {}, second is {}", x, y),
+    }
+
+    let character = 'c';
+
+    match character {
+        'a' | 'b' | 'c' => println!("Found a, b, or c"),
+        _ => println!("Other character"),
+    }
+
+    let number = -1;
+
+    match number {
+        n if n < 0 => println!("Negative number"),
+        n if n == 0 => println!("Zero"),
+        n => println!("Positive number: {}", n),
+    }
+
+    let random_value = "hello";
+
+    match random_value {
+        "hello" => println!("Hello there!"),
+        "world" => println!("World!"),
+        _ => println!("Unknown greeting"),
+    }
+
+    let list = vec![1, 2, 3];
+
+    match list.as_slice() {
+        [1, 2, ..] => println!("Starts with 1, 2"),
+        [1, ..] => println!("Starts with 1"),
+        [] => println!("Empty list"),
+        _ => println!("Some other list"),
     }
 }
