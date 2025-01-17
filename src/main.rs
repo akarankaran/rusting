d97e3a54500499dd2433ed0e350b8cc7ba1c764e
Karan <karan146panchal@gmail.com>
2025-01-17 17:08:31 +0530

    rust basics

--- src/main.rs
+++ src/main.rs
@@ -1,36 +1,21 @@
-use std::fs::File;
-use std::io::{self, Read};
+use serde::{Deserialize, Serialize};
+use serde_json::json;
 
-enum CustomError {
-    IoError(io::Error),
-    OtherError(String),
+#[derive(Serialize, Deserialize, Debug)]
+struct Config {
+    field1: Option<String>,
+    field2: Option<i32>,
+    field3: Option<bool>,
 }
 
-fn read_file_content(file_path: &str) -> Result<String, CustomError> {
-    let mut file = File::open(file_path).map_err(CustomError::IoError)?;
-    let mut content = String::new();
-    file.read_to_string(&mut content).map_err(CustomError::IoError)?;
-    Ok(content)
-}
-
-fn main() {
-    match read_file_content("example.txt") {
-        Ok(content) => println!("File content: {}", content),
-        Err(CustomError::IoError(e)) => println!("I/O error occurred: {}", e),
-        Err(CustomError::OtherError(msg)) => println!("An error occurred: {}", msg),
+impl Config {
+    fn default() -> Self {
+        Config {
+            field1: Some("default_value".to_string()),
+            field2: Some(42),
+            field3: Some(true),
+        }
     }
 
-    let result: Result<i32, &str> = divide(10, 0);
-    match result {
-        Ok(value) => println!("Result of division: {}", value),
-        Err(e) => println!("Error occurred: {}", e),
-    }
-}
-
-fn divide(numerator: i32, denominator: i32) -> Result<i32, &'static str> {
-    if denominator == 0 {
-        Err("Cannot divide by zero")
-    } else {
-        Ok(numerator / denominator)
-    }
-}
+    fn with_defaults(self) -> Self {
+        Config
