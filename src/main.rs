ac900ada68d9fddcb7d5b753df0402e5def93ab5
Karan <karan146panchal@gmail.com>
2025-01-17 16:34:28 +0530

    rust basics

--- src/main.rs
+++ src/main.rs
@@ -1,11 +1,37 @@
 fn main() {
-    let numbers = vec![1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
-    let transformed: Vec<i32> = numbers.iter().map(|&x| match x % 2 {
-        1 => x * 2,
-        _ => x,
-    }).collect();
-    
-    for number in transformed {
-        println!("{}", number);
+    let vec = vec![1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
+
+    for window in vec.windows(4) {
+        println!("{:?}", window);
     }
+
+    let slice = &vec[2..8];
+    println!("{:?}", slice);
+
+    let filtered: Vec<_> = vec.iter().filter(|&&x| x % 2 == 0).collect();
+    println!("{:?}", filtered);
+
+    if let Some(third) = vec.get(2) {
+        println!("{}", third);
+    }
+
+    let reversed: Vec<_> = vec.iter().rev().collect();
+    println!("{:?}", reversed);
+
+    let sum: i32 = vec.iter().sum();
+    println!("{}", sum);
+
+    let mut slices: Vec<&[i32]> = vec.chunks(3).collect();
+    println!("{:?}", slices);
+
+    let first_two = &vec[..2];
+    println!("{:?}", first_two);
+
+    let last_three = &vec[7..];
+    println!("{:?}", last_three);
+
+    let mut max_number = *vec.iter().max().unwrap();
+    println!("{}", max_number);
+    let index = vec.iter().position(|&x| x == max_number).unwrap();
+    println!("{}", index);
 }
