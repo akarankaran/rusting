b1749d36b89f8e1a7756e66e3db321b6eb589802
Karan <karan146panchal@gmail.com>
2025-01-17 12:42:12 +0530

    rust basics

--- src/main.rs
+++ src/main.rs
@@ -1,27 +1,15 @@
-use std::collections::HashSet;
+use std::vec::Vec;
 
 fn main() {
-    let set_a: HashSet<_> = [1, 2, 3, 4, 5].iter().cloned().collect();
-    let set_b: HashSet<_> = [4, 5, 6, 7, 8].iter().cloned().collect();
-    
-    let difference: HashSet<_> = set_a.difference(&set_b).collect();
-    println!("Difference of set_a from set_b: {:?}", difference);
+    let coordinates: Vec<(f64, f64)> = vec![
+        (1.0, 2.0),
+        (3.5, 4.5),
+        (6.1, 7.0),
+        (0.0, 0.0),
+        (8.4, 5.6),
+    ];
 
-    let set_c: HashSet<_> = ["apple", "banana", "cherry"].iter().cloned().collect();
-    let set_d: HashSet<_> = ["banana", "kiwi", "mango"].iter().cloned().collect();
-    
-    let difference_strings: HashSet<_> = set_c.difference(&set_d).collect();
-    println!("Difference of set_c from set_d: {:?}", difference_strings);
-
-    let set_e: HashSet<_> = [10, 20, 30].iter().cloned().collect();
-    let set_f: HashSet<_> = [1, 10, 20].iter().cloned().collect();
-    
-    let difference_numeric: HashSet<_> = set_e.difference(&set_f).collect();
-    println!("Difference of set_e from set_f: {:?}", difference_numeric);
-
-    let set_g: HashSet<_> = ["red", "blue", "green"].iter().cloned().collect();
-    let set_h: HashSet<_> = ["yellow", "blue", "purple"].iter().cloned().collect();
-    
-    let difference_colors: HashSet<_> = set_g.difference(&set_h).collect();
-    println!("Difference of set_g from set_h: {:?}", difference_colors);
+    for (x, y) in coordinates {
+        println!("Coordinate: ({}, {})", x, y);
+    }
 }
