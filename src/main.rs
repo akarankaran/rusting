722c9b800da8a5fed5f52a64ae651aad51672bda
Karan <karan146panchal@gmail.com>
2025-01-17 17:22:42 +0530

    rust basics

--- src/main.rs
+++ src/main.rs
@@ -1,21 +1,43 @@
-use serde::{Deserialize, Serialize};
-use serde_json::json;
+use std::vec::Vec;
 
-#[derive(Serialize, Deserialize, Debug)]
-struct Config {
-    field1: Option<String>,
-    field2: Option<i32>,
-    field3: Option<bool>,
-}
+fn main() {
+    let nested_vecs: Vec<Vec<i32>> = vec![
+        vec![1, 2, 3],
+        vec![4, 5],
+        vec![6, 7, 8, 9],
+        vec![],
+        vec![10],
+    ];
 
-impl Config {
-    fn default() -> Self {
-        Config {
-            field1: Some("default_value".to_string()),
-            field2: Some(42),
-            field3: Some(true),
+    for (i, inner_vec) in nested_vecs.iter().enumerate() {
+        println!("Vector {}: {:?}", i, inner_vec);
+        for (j, &element) in inner_vec.iter().enumerate() {
+            println!("Element at [{}][{}]: {}", i, j, element);
         }
     }
 
-    fn with_defaults(self) -> Self {
-        Config
+    let string_vecs: Vec<Vec<&str>> = vec![
+        vec!["apple", "banana"],
+        vec!["carrot"],
+        vec!["date", "elderberry", "fig"],
+    ];
+
+    for (i, inner_vec) in string_vecs.iter().enumerate() {
+        println!("String Vector {}: {:?}", i, inner_vec);
+        for (j, &element) in inner_vec.iter().enumerate() {
+            println!("Element at [{}][{}]: {}", i, j, element);
+        }
+    }
+
+    let mixed_vecs: Vec<Vec<Box<dyn std::fmt::Debug>>> = vec![
+        vec![Box::new(1), Box::new("two")],
+        vec![Box::new(3.5)],
+    ];
+
+    for (i, inner_vec) in mixed_vecs.iter().enumerate() {
+        println!("Mixed Vector {}: {:?}", i, inner_vec);
+        for (j, element) in inner_vec.iter().enumerate() {
+            println!("Element at [{}][{}]: {:?}", i, j, element);
+        }
+    }
+}
