563e0f6f4b21bba5383bac561e59ee6f79140e14
Karan <karan146panchal@gmail.com>
2025-01-17 16:54:13 +0530

    rust basics

--- src/main.rs
+++ src/main.rs
@@ -1,37 +1,36 @@
+use std::fs::File;
+use std::io::{self, Read};
+
+enum CustomError {
+    IoError(io::Error),
+    OtherError(String),
+}
+
+fn read_file_content(file_path: &str) -> Result<String, CustomError> {
+    let mut file = File::open(file_path).map_err(CustomError::IoError)?;
+    let mut content = String::new();
+    file.read_to_string(&mut content).map_err(CustomError::IoError)?;
+    Ok(content)
+}
+
 fn main() {
-    let vec = vec![1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
-
-    for window in vec.windows(4) {
-        println!("{:?}", window);
+    match read_file_content("example.txt") {
+        Ok(content) => println!("File content: {}", content),
+        Err(CustomError::IoError(e)) => println!("I/O error occurred: {}", e),
+        Err(CustomError::OtherError(msg)) => println!("An error occurred: {}", msg),
     }
 
-    let slice = &vec[2..8];
-    println!("{:?}", slice);
-
-    let filtered: Vec<_> = vec.iter().filter(|&&x| x % 2 == 0).collect();
-    println!("{:?}", filtered);
-
-    if let Some(third) = vec.get(2) {
-        println!("{}", third);
+    let result: Result<i32, &str> = divide(10, 0);
+    match result {
+        Ok(value) => println!("Result of division: {}", value),
+        Err(e) => println!("Error occurred: {}", e),
     }
+}
 
-    let reversed: Vec<_> = vec.iter().rev().collect();
-    println!("{:?}", reversed);
-
-    let sum: i32 = vec.iter().sum();
-    println!("{}", sum);
-
-    let mut slices: Vec<&[i32]> = vec.chunks(3).collect();
-    println!("{:?}", slices);
-
-    let first_two = &vec[..2];
-    println!("{:?}", first_two);
-
-    let last_three = &vec[7..];
-    println!("{:?}", last_three);
-
-    let mut max_number = *vec.iter().max().unwrap();
-    println!("{}", max_number);
-    let index = vec.iter().position(|&x| x == max_number).unwrap();
-    println!("{}", index);
+fn divide(numerator: i32, denominator: i32) -> Result<i32, &'static str> {
+    if denominator == 0 {
+        Err("Cannot divide by zero")
+    } else {
+        Ok(numerator / denominator)
+    }
 }
