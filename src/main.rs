a0ec166534bc413e22545c3a8cc94276f1a95196
Karan <karan146panchal@gmail.com>
2025-01-17 16:11:12 +0530

    rust basics

--- src/main.rs
+++ src/main.rs
@@ -1,36 +1,15 @@
+fn swap_if_too_small(tuple: (i32, i32)) -> (i32, i32) {
+    if tuple.0 < 10 && tuple.1 < 10 {
+        (tuple.1, tuple.0)
+    } else {
+        tuple
+    }
+}
+
 fn main() {
-    let ages = vec![12, 15, 20, 25, 35, 60];
-
-    for age in ages {
-        let label = match age {
-            0..=12 => "Child",
-            13..=19 => "Teen",
-            20..=34 => "Adult",
-            35..=64 => "Middle-aged",
-            _ => "Senior",
-        };
-        println!("Age: {}, Label: {}", age, label);
-    }
-
-    let ranges = vec![(0..=5, "Infant"), (6..=12, "Child"), (13..=17, "Teen"), (18..=64, "Adult"), (65..=100, "Senior")];
-
-    let age_check = 8;
-    for (range, label) in ranges {
-        if range.contains(&age_check) {
-            println!("Age: {}, Label: {}", age_check, label);
-        }
-    }
-
-    let ages_with_labels = vec![(11, "Child"), (16, "Teen"), (23, "Adult"), (40, "Middle-aged"), (70, "Senior")];
-
-    for (age, expected_label) in ages_with_labels {
-        let result_label = match age {
-            0..=12 => "Child",
-            13..=19 => "Teen",
-            20..=34 => "Adult",
-            35..=64 => "Middle-aged",
-            _ => "Senior",
-        };
-        println!("Age: {}, Expected: {}, Result: {}", age, expected_label, result_label);
+    let tuples = vec![(5, 3), (12, 8), (7, 20), (9, 1), (15, 5)];
+    let swapped_tuples: Vec<(i32, i32)> = tuples.into_iter().map(swap_if_too_small).collect();
+    for t in swapped_tuples {
+        println!("{:?}", t);
     }
 }
