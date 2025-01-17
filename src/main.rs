d837554fd1d2f37e6b20438f54b9302f1d7b360b
Karan <karan146panchal@gmail.com>
2025-01-17 16:21:19 +0530

    rust basics

--- src/main.rs
+++ src/main.rs
@@ -1,15 +1,11 @@
-fn swap_if_too_small(tuple: (i32, i32)) -> (i32, i32) {
-    if tuple.0 < 10 && tuple.1 < 10 {
-        (tuple.1, tuple.0)
-    } else {
-        tuple
-    }
-}
-
 fn main() {
-    let tuples = vec![(5, 3), (12, 8), (7, 20), (9, 1), (15, 5)];
-    let swapped_tuples: Vec<(i32, i32)> = tuples.into_iter().map(swap_if_too_small).collect();
-    for t in swapped_tuples {
-        println!("{:?}", t);
+    let numbers = vec![1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
+    let transformed: Vec<i32> = numbers.iter().map(|&x| match x % 2 {
+        1 => x * 2,
+        _ => x,
+    }).collect();
+    
+    for number in transformed {
+        println!("{}", number);
     }
 }
