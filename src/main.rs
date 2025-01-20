3c6bec63f465defce9fa7f64d51600e91ccbe1d0
Karan <karan146panchal@gmail.com>
2025-01-20 12:01:37 +0530

    rust basics

--- src/main.rs
+++ src/main.rs
@@ -1,43 +1,32 @@
-use std::vec::Vec;
+use std::fmt;
+
+trait Printable {
+    fn print(&self);
+}
+
+struct Person {
+    name: String,
+    age: u32,
+}
+
+impl Printable for Person {
+    fn print(&self) {
+        println!("Name: {}, Age: {}", self.name, self.age);
+    }
+}
 
 fn main() {
-    let nested_vecs: Vec<Vec<i32>> = vec![
-        vec![1, 2, 3],
-        vec![4, 5],
-        vec![6, 7, 8, 9],
-        vec![],
-        vec![10],
-    ];
+    let person = Person {
+        name: String::from("Alice"),
+        age: 30,
+    };
 
-    for (i, inner_vec) in nested_vecs.iter().enumerate() {
-        println!("Vector {}: {:?}", i, inner_vec);
-        for (j, &element) in inner_vec.iter().enumerate() {
-            println!("Element at [{}][{}]: {}", i, j, element);
-        }
-    }
+    person.print();
+    
+    let another_person = Person {
+        name: String::from("Bob"),
+        age: 25,
+    };
 
-    let string_vecs: Vec<Vec<&str>> = vec![
-        vec!["apple", "banana"],
-        vec!["carrot"],
-        vec!["date", "elderberry", "fig"],
-    ];
-
-    for (i, inner_vec) in string_vecs.iter().enumerate() {
-        println!("String Vector {}: {:?}", i, inner_vec);
-        for (j, &element) in inner_vec.iter().enumerate() {
-            println!("Element at [{}][{}]: {}", i, j, element);
-        }
-    }
-
-    let mixed_vecs: Vec<Vec<Box<dyn std::fmt::Debug>>> = vec![
-        vec![Box::new(1), Box::new("two")],
-        vec![Box::new(3.5)],
-    ];
-
-    for (i, inner_vec) in mixed_vecs.iter().enumerate() {
-        println!("Mixed Vector {}: {:?}", i, inner_vec);
-        for (j, element) in inner_vec.iter().enumerate() {
-            println!("Element at [{}][{}]: {:?}", i, j, element);
-        }
-    }
+    another_person.print();    
 }
